676e2fce16e267e460f82ab5d196321af3e5eb5f
li_lingyi <centreis@xiaoice.vsinger.com._.local>
2025-09-25 20:12:07 +0800

    fix wrong id

--- modules/models.py
+++ modules/models.py
@@ -4,7 +4,7 @@ from pgvector.sqlalchemy import Vector
 
 class ChatMessageModel(vt.orm.Base):
     __tablename__ = "chat_message"
-    seq: int = sa.Column(sa.BigInteger(), default=vt.algo.calc_seqid(), primary_key=True)
+    seq: int = sa.Column(sa.BigInteger(), default=vt.algo.calc_seqid, primary_key=True)
     bot: str = sa.Column(sa.String(100), primary_key=True)
     user: int = sa.Column(sa.BigInteger(), primary_key=True)
     query: str = sa.Column(sa.Text(), nullable=False)
